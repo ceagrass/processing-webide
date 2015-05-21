bbae0092bee88954a8edef6686c81785514543d5
sea-grass <dev.seagrass@gmail.com>
2015-05-21 19:57:30 -0400

    Refactor app.js code
    	It is now much cleaner. It is surprising to see how much unnecessary code
    	was in place because of my insistence to use closures. You live and you
    	learn, I guess.

--- app.js
+++ app.js
@@ -1,132 +1,90 @@
-var app = function() {
-  var container;
-  container = document.getElementById("container");
-	var localStorage_key = "pide_code";
-
-	/*sendToConsole is an optional boolean argument; if true then send message to console instead*/
-	var debug = function() {
-    var debugEl;
-    debugEl = document.getElementById("debug");
-		return function(message, sendToConsole) {
-			if (sendToConsole) {
-				console.log(message);
-			}
-			else {
-				//Add message to debug history
-				//TODO
-				//Display message in debugEl
-				debugEl.innerText += message;	
-			}
-		};
-	}();
-	return {
-		container: container,
-		debug: debug,
-		LS_KEY: localStorage_key
-	};
-}();
-
-/*Make sure that the app can be started properly*/
-var errorOccured = false;
-if (Processing) {
-	app.debug("Processing.js loaded.");
-} else {
-	errorOccured = true;
-}
-if (window.localStorage) {
-	app.debug("localStorage is available.");
-} else {
-	errorOccured = true;
-}
-//Handle error
-if (errorOccured) {
-	app.debug("Error initializing the app. Your browser may be unsupported.");
-}
-
-
-//Need an inputArea; somewhere to type processing code
-app.inputArea = function(inputEl) {
-	function get() {
-		var content = "";
-		content += inputEl.value;
-		return content;
-	}
-	function set(content) {
-		inputEl.value = content;
-		return content;
-	}
-	var saveButton = function(el) {
-		el.innerText = "Save & Load sketch";
-		el.addEventListener("click", function(e) {
-			app.saveCode(get());
-			window.location.href = window.location.href;
-			app.loadCode();
-		});
-
-		app.container.appendChild(el);
-		return el;
-	}(document.getElementById("saveAndLoad"));
-
-	return {
-		el: inputEl,
-		get: function() {
-			var content = "";
-			content += inputEl.value;
-			return content;
-		},
-		set: function(content) {
-			inputEl.value = content;
-			return content;
-		}
-	}
-}(document.getElementById("fileInput"));
-//Need an outputArea; somewhere to display the processing output
-app.outputArea = function(outputEl) {
-  var outputContainer;
-  outputContainer = document.getElementById("preview");
-	outputContainer.appendChild(outputEl);
-
-	outputEl.id = "myCanvas";
-
-	return {
-		el: outputEl,
-		get: function() {
-			//Get the canvas contents as base64 image
-			//TODO
-		},
-		set: function(processingCode, cnvEl) {
-			if (!cnvEl) {
-				var cnvEl = outputEl;
-			}
-			if (cnvEl.id == undefined || cnvEl.id == "") {
-				cnvEl.id = "pSketch" + (Math.random()*100).toFixed();
-
-			}
-			//Load the processingCode string into the canvas el
-			console.log(processingCode);
-			//Create a sketch from the code
-			var sketch = Processing.compile(processingCode);
-			//Start the sketch, binding it to the canvas element
-			var processingInstance = new Processing(cnvEl.id, sketch);
-			return processingInstance;
-		}
-	};
-}(document.createElement("canvas"));
-
+var app = {};
+app.container = document.getElementById("container");
+app.LS_KEY = "pide_code";
 app.saveCode = function() {
-	return function() {
-		window.localStorage.setItem(app.LS_KEY, app.inputArea.get());
-	}
-}();
+  window.localStorage.setItem(app.LS_KEY, app.inputArea.get());
+};
 app.loadCode = function() {
-	if (window.localStorage.getItem(app.LS_KEY) == null) {
-		window.localStorage.setItem(app.LS_KEY, "");
-	}
-	return function() {
-		return window.localStorage.getItem(app.LS_KEY);
-	}
-}();
+  return window.localStorage.getItem(app.LS_KEY);
+};
+app.debug = (function() {
+  var debugEl = document.getElementById("debug");
 
+  return function(message, sendToConsole) {
+    if (sendToConsole) {
+      console.log(message);
+    } else {
+      debugEl.innerText += message;
+    }
+  };
+}());
+
+// Make sure that the app can be started properly
+var required = {
+  "processing": Processing ? true : false,
+  "localStorage": window.localStorage ? true : false,
+  "Worker": !!window.Worker ? true : false
+};
+for (var key in required) {
+  if (!required[key]) {
+    app.debug(key + " failed to load.");
+  } else {
+    app.debug(key + " loaded.");
+  }
+}
+
+app.inputArea = {};
+app.inputArea.el = document.getElementById("fileInput");
+app.inputArea.get = function() {
+  return "" + app.inputArea.el.value;
+};
+app.inputArea.set = function(content) {
+  app.inputArea.el.value = content;
+
+  return content;
+};
+
+var saveButton = document.getElementById("saveAndLoad");
+saveButton.addEventListener("click", function(e) {
+  app.saveCode(app.inputArea.get());
+  //Refresh the page
+  window.location.href = "";
+});
+
+//Need an outputArea; somewhere to display the processing output
+app.outputArea = {};
+app.outputArea.container = document.getElementById("preview");
+app.outputArea.el = document.createElement("canvas");
+//The output element needs an id or else processingjs doesn't work
+app.outputArea.el.id = "myCanvas";
+app.outputArea.get = function() {
+  //TODO: Get the canvas contents as base64 image
+};
+app.outputArea.set = function(processingCode, cnvEl) {
+  if (!cnvEl) {
+    cnvEl = app.outputArea.el;
+  }
+  if (cnvEl.id === undefined || cnvEl.id === "") {
+    //Again, processingjs needs the canvas to have an id
+    cnvEl.id = "pSketch" + (Math.random() * 100).toFixed();
+  }
+
+  console.log(processingCode);
+  //Create a sketch from the code
+  var sketch = Processing.compile(processingCode);
+  //TODO: Check sketch for errors
+  //Add the canvas to the container before the instance is created
+  app.outputArea.container.appendChild(cnvEl);
+  //Start the sketch, binding it to the canvas element
+  var processingInstance = new Processing(cnvEl.id, sketch);
+
+  return processingInstance;
+};
+
+//Make sure that the localStorage[key] is not null
+if (window.localStorage.getItem(app.LS_KEY) === null) {
+  window.localStorage.setItem(app.LS_KEY, "");
+}
 //START THE APP by loading the last saved processing code from localStorage
 var code = app.loadCode();
 //Load the processing code from localStorage into the textArea
